a0d3543f1e2224c523f4fd1da4f2d96d5b4f1fa5
229935 <229935@edu.p.lodz.pl>
2021-12-11 16:58:57 +0100

    rest api dla usera testowe

---
+++ src/main/java/Webservices/AppConfig.java
@@ -0,0 +1,8 @@
+package Webservices;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("record-shop")
+public class AppConfig extends Application {
+}
--- src/main/java/Webservices/UserWebservice.java
+++ src/main/java/Webservices/UserWebservice.java
@@ -1,5 +1,20 @@
 package Webservices;
 
+import Model.User;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("user")
 public class UserWebservice {
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUser(){
+        User u = new User(1, "peja997");
+        u.setActive(true);
+        return u;
+    }
 }
