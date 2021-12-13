b18bb9af18fb63639f5217aa95cb8dc3fd55bd11
mdzieciuchowicz <229874@edu.p.lodz.pl>
2021-12-13 20:53:27 +0100

    fix rents

--- src/main/java/Model/Managers/RecordManager.java
+++ src/main/java/Model/Managers/RecordManager.java
@@ -1,19 +1,14 @@
 package Model.Managers;
 
-import Model.Exceptions.BasicException;
-import Model.Exceptions.InputException;
 import Model.Exceptions.NotFoundException;
 import Model.Exceptions.RentalException;
 import Model.Record;
 import Model.Repositories.RecordRepository;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
+
 
 @ApplicationScoped
 public class RecordManager {
