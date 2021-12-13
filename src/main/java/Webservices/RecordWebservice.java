fda617f3fb75816e85a34502104b45c886020486
mdzieciuchowicz <229874@edu.p.lodz.pl>
2021-12-13 21:54:51 +0100

    fix RecordWebservice

--- src/main/java/Webservices/RecordWebservice.java
+++ src/main/java/Webservices/RecordWebservice.java
@@ -1,5 +1,4 @@
 package Webservices;
-import Model.Exceptions.BasicException;
 import Model.Exceptions.InputException;
 import Model.Exceptions.NotFoundException;
 import Model.Exceptions.RentalException;
@@ -26,16 +25,20 @@ public class RecordWebservice {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response getRecord(@QueryParam("recordid") String recordid) {
-        if (recordid != null) {
-            try {
-                Record recordFound = recordManager.getRecordByID(recordid);
-                return Response.ok(recordFound).build();
-            } catch (NotFoundException e) {
-                return Response.status(404).entity(e).build();
-            }
-        } else {
-            return Response.ok(recordManager.getAllRecords()).build();
+    public Response getRecords() {
+        return Response.ok(recordManager.getAllRecords()).build();
+    }
+
+    @GET
+    @Path("{recordID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getRecord(@PathParam("recordID") String recordID) {
+        try {
+            Record recordFound = recordManager.getRecordByID(recordID);
+            return Response.ok(recordFound).build();
+        } catch (NotFoundException e) {
+            return Response.status(404).entity(e).build();
         }
     }
 
@@ -68,12 +71,13 @@ public class RecordWebservice {
     }
 
     @DELETE
+    @Path("{recordID}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response deleteRecord(@QueryParam("recordid") String recordid) {
+    public Response deleteRecord(@PathParam("recordID") String recordID) {
         try {
-            Record record = recordManager.getRecordByID(recordid);
-            recordManager.removeRecord(recordid);
+            Record record = recordManager.getRecordByID(recordID);
+            recordManager.removeRecord(recordID);
             return Response.ok(record).build();
         } catch (RentalException e) {
             return Response.status(400).entity(e).build();
@@ -83,23 +87,22 @@ public class RecordWebservice {
     }
 
     @POST
-    @Path("modify")
+    @Path("{recordID}/edit")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response modifyRecord(String body) {
+    public Response modifyRecord(@PathParam("recordID") String recordID, String body) {
         try {
             JsonObject jsonBody = JsonParser.parseString(body).getAsJsonObject();
 
-            String recordid = jsonBody.get("recordid").getAsString();
-            if (!recordid.matches("\\b[0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b")) {
+            if (!recordID.matches("\\b[0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b")) {
                 throw new InputException("Wrong uuid format");
             }
 
-            Record record = recordManager.getRecordByID(recordid);
+            Record record = recordManager.getRecordByID(recordID);
 
             String title = jsonBody.get("title").getAsString();
             if (title.length() != 0) {
-                if (!title.matches("^[a-z0-9_-]{3,50}$")) {
+                if (!title.matches("^[a-zA-Z0-9_ -]{3,50}$")) {
                     throw new InputException("Title must be between 3 and 50 characters");
                 }
 
@@ -108,7 +111,7 @@ public class RecordWebservice {
 
             String artist = jsonBody.get("artist").getAsString();
             if (artist.length() != 0) {
-                if (!artist.matches("^[a-z0-9_-]{3,50}$")) {
+                if (!artist.matches("^[a-zA-Z0-9_ -]{3,50}$")) {
                     throw new InputException("Artist name must be between 3 and 50 characters");
                 }
 
